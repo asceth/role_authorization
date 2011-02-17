91f686e95f4c7b723f51e2c83191265d9a566023
asceth <machinist@asceth.com>
2011-02-17 15:17:34 -0500

    dont load controller classes

--- lib/rails/role_authorization.rb
+++ lib/rails/role_authorization.rb
@@ -2,7 +2,6 @@ module RoleAuthorization
   class Railtie < Rails::Railtie
     initializer "role_authorization.initialize" do |app|
       RoleAuthorization.enable
-      RoleAuthorization.load_controller_classes
     end
   end
 end
