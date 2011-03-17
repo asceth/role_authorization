4d9fde207f5a01a12955a69571b50258df4d5601
John Long <machinist@asceth.com>
2011-03-17 14:43:56 -0400

    change rules to instance_eval on controller instance for better proc readaibility in scopes/resources

--- lib/role_authorization/rules/defaults.rb
+++ lib/role_authorization/rules/defaults.rb
@@ -8,7 +8,7 @@ RoleAuthorization::Rules.define :role do
   else
     scope = if options[:scope]
               if options[:scope].is_a?(Proc)
-                instance_eval(&options[:scope])
+                controller_instance.instance_eval(&options[:scope])
               else
                 options[:scope]
               end
@@ -21,18 +21,22 @@ RoleAuthorization::Rules.define :role do
 end
 
 RoleAuthorization::Rules.define :user do
-  resource = controller_instance.instance_variable_get("@#{options[:resource]}".to_sym)
-
-  if resource.nil?
+  if controller_instance.current_user.blank?
     false
   else
-    controller_instance.current_user.try(:id) == resource.try(options[:check])
+    if options[:resource]
+      check_method = options[:check] || :id
+      resource_instance = controller_instance.instance_eval(&options[:resource])
+      controller_instance.current_user.id == resource_instance.try(check_method)
+    else
+      false
+    end
   end
 end
 
 RoleAuthorization::Rules.define :custom do
   unless options[:block].nil?
-    true if options[:block].call(controller_instance) == true
+    true if controller_instance.instance_eval(&options[:block]) == true
   else
     false
   end
